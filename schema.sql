a42cd87c462c9ba93cb224aa6e936c8bd2acb7d4
AcidBurst7 <varaksinalla@rambler.ru>
2020-09-13 23:02:30 +0300

    Схема базы данных

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE taskforce
-    DEFAULT CHARACTER SET utf-8
+    DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
 
 USE taskforce;
@@ -12,8 +12,8 @@ CREATE TABLE users (
     address VARCHAR(500),
     birth_date DATETIME,
     rating FLOAT(1,1) NOT NULL,
-    description VARCHAR(1000)
-    phone CHAR(11) UNIQUE,
+    description VARCHAR(1000),
+    phone CHAR(11),
     skype CHAR(35),
     another_messenger CHAR(35),
     image_link CHAR(150),
@@ -29,11 +29,7 @@ CREATE TABLE tasks (
     location_id INT(11) NOT NULL,
     status_id INT(11) NOT NULL,
     date_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_finish DATETIME NOT NULL,
-
-    FOREIGN KEY (category_id) REFERENCES tasks_categories(id),
-    FOREIGN KEY (location_id) REFERENCES locations(id),
-    FOREIGN KEY (status_id) REFERENCES tasks_statuses(id)
+    date_finish DATETIME NOT NULL
 );
 
 CREATE TABLE tasks_statuses (
@@ -44,9 +40,7 @@ CREATE TABLE tasks_statuses (
 CREATE TABLE tasks_files (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     task_id INT(11),
-    file_link CHAR(250),
-
-    FOREIGN KEY (task_id) REFERENCES tasks(id)
+    file_link CHAR(250)
 );
 
 CREATE TABLE tasks_categories (
@@ -57,10 +51,7 @@ CREATE TABLE tasks_categories (
 CREATE TABLE users_tasks (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     task_id INT(11) NOT NULL,
-    user_id INT(11) NOT NULL,
-
-    FOREIGN KEY (task_id) REFERENCES tasks(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id INT(11) NOT NULL
 );
 
 CREATE TABLE specializations (
@@ -71,10 +62,7 @@ CREATE TABLE specializations (
 CREATE TABLE users_specializations (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     specialization_id INT(11),
-    user_id INT(11),
-
-    FOREIGN KEY (specialization_id) REFERENCES specializations(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id INT(11)
 );
 
 CREATE TABLE notifications (
@@ -85,25 +73,42 @@ CREATE TABLE notifications (
 CREATE TABLE users_notifications (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     notification_id INT(11),
-    user_id INT(11),
-
-    FOREIGN KEY (notification_id) REFERENCES notifications(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id INT(11)
 );
 
 CREATE TABLE locations (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(350) NOT NULL,
-    lat FLOAT(2, 7) NOT NULL,
-    lng FLOAT(2, 7) NOT NULL
+    lat DOUBLE(8, 7),
+    lng DOUBLE(8, 7)
 );
 
 CREATE TABLE reviews (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     text VARCHAR(500),
     user_id INT(11) NOT NULL,
-    task_id INT(11) NOT NULL,
-
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (task_id) REFERENCES tasks(id)
+    task_id INT(11) NOT NULL
 );
+
+CREATE TABLE dialogs (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT(11) NOT NULL,
+    receiver_id INT(11) NOT NULL,
+    task_id INT(11) NOT NULL
+);
+
+ALTER TABLE tasks ADD FOREIGN KEY (category_id) REFERENCES tasks_categories(id);
+ALTER TABLE tasks ADD FOREIGN KEY (location_id) REFERENCES locations(id);
+ALTER TABLE tasks ADD FOREIGN KEY (status_id) REFERENCES tasks_statuses(id);
+ALTER TABLE tasks_files ADD FOREIGN KEY (task_id) REFERENCES tasks(id);
+ALTER TABLE users_tasks ADD FOREIGN KEY (task_id) REFERENCES tasks(id);
+ALTER TABLE users_tasks ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE users_specializations ADD FOREIGN KEY (specialization_id) REFERENCES specializations(id);
+ALTER TABLE users_specializations ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE users_notifications ADD FOREIGN KEY (notification_id) REFERENCES notifications(id);
+ALTER TABLE users_notifications ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE reviews ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE reviews ADD FOREIGN KEY (task_id) REFERENCES tasks(id);
+ALTER TABLE dialogs ADD FOREIGN KEY (sender_id) REFERENCES users(id);
+ALTER TABLE dialogs ADD FOREIGN KEY (receiver_id) REFERENCES users(id);
+ALTER TABLE dialogs ADD FOREIGN KEY (task_id) REFERENCES tasks(id);
